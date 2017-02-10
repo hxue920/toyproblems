e70eb523e9ecd55178ea14d4b1126c361ea5b43f
hxue920 <devilsing@gmail.com>
2017-02-09 23:34:08 -0800

    implement getAncestorPath

--- src/week01.js
+++ src/week01.js
@@ -105,3 +105,89 @@ function spiralTraversal (matrix) {
   return result;
 }
 
+var Tree = function(){
+  this.children = [];
+};
+
+/**
+  * add an immediate child
+  */
+Tree.prototype.addChild = function(child){
+  if(!this.isDescendant(child)){
+    this.children.push(child);
+  }else {
+    throw new Error("That child is already a child of this tree");
+  }
+  return this;
+};
+
+Tree.prototype.getClosestCommonAncestor = function(){
+  // TODO: implement me!
+  // var result;
+  // var args = Array.prototype.slice.call(arguments);
+  // for (var i = 0; i < args.length; i++) {
+  //   if ()
+  // }
+}
+
+Tree.prototype.getAncestorPath = function(target){
+  // TODO: implement me!
+  if (this === target) {
+    return [this];
+  }
+  for (var i = 0; i < this.children.length; i++) {
+    var path = this.children[i].getAncestorPath(target);
+    if (path) {
+      return [this].concat(path);
+    }
+  }
+  // var result = [];
+  // function findSolution(tree) {
+  //   if (tree === target) {
+  //     result.push(tree);
+  //     return;
+  //   }
+
+  //   for (var i = 0; i < tree.children.length; i++) {
+  //     if (tree.children[i].isDescendant(target)) {
+  //       result.push(tree.children[i]);
+  //       findSolution(tree.children[i])
+  //     }
+  //   }
+  // }
+  // findSolution(this);
+  // return result;
+  return null;
+}
+
+/**
+  * check to see if the provided tree is already a child of this
+  * tree __or any of its sub trees__
+  */
+Tree.prototype.isDescendant = function(child){
+  if(this.children.indexOf(child) !== -1){
+    // `child` is an immediate child of this tree
+    return true;
+  }else{
+    for(var i = 0; i < this.children.length; i++){
+      if(this.children[i].isDescendant(child)){
+        // `child` is descendant of this tree
+        return true;
+      }
+    }
+    return false;
+  }
+};
+
+/**
+  * remove an immediate child
+  */
+Tree.prototype.removeChild = function(child){
+  var index = this.children.indexOf(child);
+  if(index !== -1){
+    // remove the child
+    this.children.splice(index,1);
+  }else{
+    throw new Error("That node is not an immediate child of this tree");
+  }
+};
