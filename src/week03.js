461a45cf5bb01ed57645d14c40c4ba0b9c4bd059
hxue920 <devilsing@gmail.com>
2017-02-23 18:31:38 -0800

    fix decode function not return correct character

--- src/week03.js
+++ src/week03.js
@@ -29,4 +29,27 @@ var encode = function(str) {
 
 var decode = function() {
   //enter function
+  var position = 0;
+  var strArr = str.split("");
+  var firstAlpha = false;
+  var results = strArr.map(function(char) {
+    if (charObj[char.toUpperCase()]) {
+      if (!firstAlpha) {
+        firstAlpha = true;
+        position = charObj[char.toUpperCase()];
+        return char.toUpperCase();
+      }
+      var move = charObj[char.toUpperCase()] - position < 0 ? 26 + charObj[char.toUpperCase()] - position : charObj[char.toUpperCase()] - position;
+      for (key in charObj) {
+          if (charObj[key] === move) {
+            char = key;
+          }
+      }
+      position = charObj[char.toUpperCase()];
+    }
+    return char;
+  });
+
+  results = results.join("");
+  return results;
 }
