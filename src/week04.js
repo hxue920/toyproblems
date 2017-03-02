d33cc0afed44cffaccdcdf871fbee2f6e9d32218
hxue920 <devilsing@gmail.com>
2017-03-01 20:43:49 -0800

    implement findOdd

--- src/week04.js
+++ src/week04.js
@@ -14,3 +14,18 @@ class SmallestIntegerFinder {
       return args.sort(function(a,b){return b - a}).pop();
   }
 }
+
+function findOdd(A) {
+  var obj = A.reduce(function(prev, curr) {
+    if (!prev[curr]) {
+      prev[curr] = 0;
+    }
+    prev[curr] += 1;
+    return prev;
+  }, {});
+  for (key in obj) {
+    if (obj[key] % 2 !==0) {
+      return parseInt(key);
+    }
+  }
+}
