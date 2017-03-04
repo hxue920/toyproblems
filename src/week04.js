dbc7949397ca10a0824c9bcebe805f448166e064
hxue920 <devilsing@gmail.com>
2017-03-03 21:45:59 -0800

    implement initial comp function

--- src/week04.js
+++ src/week04.js
@@ -29,3 +29,26 @@ function findOdd(A) {
     }
   }
 }
+
+function pigIt(str){
+  //Code here
+  return str.split(" ").map(function(word) {
+
+    var wordArr = word.split("");
+    console.log(wordArr);
+    var newTail = wordArr.shift().concat('ay');
+
+    wordArr.push(newTail);
+    console.log(wordArr.join(""));
+    return wordArr.join("");
+  }).join(" ");
+}
+a1 = [121, 144, 19, 161, 19, 144, 19, 11];
+a2 = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19];
+function comp(array1, array2){
+  return (array1===null || array2===null) ? false : array2.every(function(element) {
+    console.log(element);
+    return array1.includes(Math.sqrt(element));
+  });
+}
+console.log(comp(null, null));
