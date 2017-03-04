1184a48cb10a2b115998c81368b75fad4a1a2edd
hxue920 <devilsing@gmail.com>
2017-03-03 21:46:34 -0800

    refactor comp function

--- src/week04.js
+++ src/week04.js
@@ -43,12 +43,6 @@ function pigIt(str){
     return wordArr.join("");
   }).join(" ");
 }
-a1 = [121, 144, 19, 161, 19, 144, 19, 11];
-a2 = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19];
 function comp(array1, array2){
-  return (array1===null || array2===null) ? false : array2.every(function(element) {
-    console.log(element);
-    return array1.includes(Math.sqrt(element));
-  });
+  return !array1 || !array2 ? false : JSON.stringify(array1.sort(function(a,b) {return a - b})) === JSON.stringify(array2.map(function(ele) {return Math.sqrt(ele)}).sort(function(a,b) {return a - b}))
 }
-console.log(comp(null, null));
