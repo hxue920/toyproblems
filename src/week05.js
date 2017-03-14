2ae230a0312b69def18b041f1f4171e1cde2affb
hxue920 <devilsing@gmail.com>
2017-03-13 23:42:30 -0700

    implement partial humanReadable function

--- src/week05.js
+++ src/week05.js
@@ -9,6 +9,24 @@ function add(n){
 
   return fn;
 }
-PaginationHelper
+// PaginationHelper
 
-Human Readable Time
+function humanReadable(seconds) {
+  // TODO
+  var leftover = seconds;
+  var reminder, sec, min, hour;
+  var arr = [60,60,100];
+  for (var i = 0; i < arr.length; i++) {
+    if (leftover >= arr[i]) {
+      var temp = Math.floor(leftover / arr[i]);
+      arr[i] = leftover % arr[i];
+      leftover = temp;
+    } else {
+      arr[i] = leftover;
+      leftover = 0;
+    }
+  }
+  return arr;
+}
+
+console.log(humanReadable(60));
