e409a445efadff6eafa1410ce841e3e970554568
hxue920 <devilsing@gmail.com>
2017-04-08 22:03:38 -0700

    try different approach for helloWrold

--- src/week09.js
+++ src/week09.js
@@ -6,15 +6,21 @@ var helloWorld = function () {
   //   o : true,
 
   // }
-  var H = true;
-  var e = true;
-  var l = true;
-  var o = true;
-  console.log(H.toString()+e.toString() + l.toString() + o.toString());
-  // return new String(Object.keys(obj).join(""));
-  var a = new String('Hello');
-  var b = new String(' World!');
-  return a + b;
+  // var H = true;
+  // var e = true;
+  // var l = true;
+  // var o = true;
+  // console.log(H.toString()+e.toString() + l.toString() + o.toString());
+  // // return new String(Object.keys(obj).join(""));
+  // var a = new String('Hello');
+  // var b = new String(' World!');
+  // return a + b;
+  var H = function() {};
+  var e = function() {};
+  var l = function() {};
+  var o = function() {};
+  var w = function() {};
+  return H.name + e.name + l.name + l.name + o.name;
 }
 
 console.log(helloWorld());
