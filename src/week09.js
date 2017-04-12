0ed0602b89f363e2c118b5c236b45b40f62be2fe
hxue920 <devilsing@gmail.com>
2017-04-11 22:26:02 -0700

    init functional lists

--- src/week09.js
+++ src/week09.js
@@ -64,3 +64,31 @@ function sumStrings(a,b) {
   }
   return result.map(function(num) {return num.toString();}).join("");
 }
+
+
+function List() {}
+
+function EmptyList() {}
+EmptyList.prototype = new List();
+EmptyList.prototype.constructor = EmptyList;
+
+EmptyList.prototype.toString = function() { /* implement this */ };
+EmptyList.prototype.isEmpty = function() { /* implement this */ };
+EmptyList.prototype.length = function() { /* implement this */ };
+EmptyList.prototype.push = function(x) { /* implement this */ };
+EmptyList.prototype.remove = function(x) { /* implement this */ };
+EmptyList.prototype.append = function(xs) { /* implement this */ };
+
+function ListNode(value, next) { /* implement this */ }
+ListNode.prototype = new List();
+ListNode.prototype.constructor = ListNode;
+ListNode.prototype.isEmpty = function() { /* implement this */ };
+
+ListNode.prototype.toString = function() { /* implement this */ };
+
+ListNode.prototype.head = function() { /* implement this */ };
+ListNode.prototype.tail = function() { /* implement this */  };
+ListNode.prototype.length = function() { /* implement this */ };
+ListNode.prototype.push = function(x) { /* implement this */ };
+ListNode.prototype.remove = function(x) { /* implement this */ };
+ListNode.prototype.append = function(xs) { /* implement this */ };
