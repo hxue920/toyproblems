b85e353168830f601cf3c5678be5601c371debc5
hxue920 <devilsing@gmail.com>
2017-04-20 16:24:42 -0700

    add logic to modify functions inside chain returned object

--- src/week10.js
+++ src/week10.js
@@ -1,8 +1,35 @@
 function chain(fns) {
   var obj = fns;
-
-  obj.execute = function(value) {
-    return value;
+  for (var fn in obj) {
+    obj[fn] = function() {
+      obj.value = obj[fn];
+      return this;
+    }
+  }
+  obj[fn].bind(this, this.value || null);
+  obj.value = 0;
+  obj.execute = function() {
+    return this.value
   }
   return obj;
 }
+
+function sum(x, y) {
+  return x + y;
+}
+
+function double(x) {
+  return sum(x, x);
+}
+
+function minus (x, y) {
+  return x - y;
+}
+
+function addOne(x) {
+  return sum(x, 1);
+}
+
+var c = chain({sum: sum, minus: minus, double: double, addOne: addOne});
+
+console.log(c.sum(1,2).execute());
