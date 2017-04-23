6cff235bb780fb7b3a6ea9f1fcb415adec322b2f
hxue920 <devilsing@gmail.com>
2017-04-22 20:52:29 -0700

    add implementation for loop_size function

--- src/week10.js
+++ src/week10.js
@@ -2,8 +2,15 @@ function chain(fns) {
   var obj = fns;
   obj.value = null;
   for (var fn in obj) {
+    var temp = obj[fn];
+    console.log(temp);
     obj[fn] = function() {
-      this.value ? (obj[fn].bind(this, obj.value))() : obj[fn].bind(this)();
+      if (this.value) {
+        console.log(temp);
+        this.value = temp.bind(this, this.value)();
+      } else {
+        this.value = temp.apply(this, arguments);
+      }
       return this;
     }
     // obj[fn] = obj[fn].bind(this, null);
@@ -34,3 +41,25 @@ function addOne(x) {
 var c = chain({sum: sum, minus: minus, double: double, addOne: addOne});
 
 console.log(c.sum(1,2));
+
+
+function loop_size(node){
+  console.log(node);
+  var fastNode = node;
+  var slowNode = node;
+  var count = 0;
+
+  while (fastNode.next.next !== null) {
+    fastNode = fastNode.next.next;
+    slowNode = slowNode.next;
+
+    if (slowNode === fastNode) {
+      break;
+    }
+  }
+  while (fastNode !== slowNode) {
+    fastNode = fastNode.next;
+    count += 1;
+  }
+  return count - 1;
+}
