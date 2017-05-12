7c51fba7a3a77717b3e26efd3ac42caa4a011fd0
hxue920 <devilsing@gmail.com>
2017-05-11 17:23:14 -0700

    add implementation for MedianStream

--- src/week11.js
+++ src/week11.js
@@ -31,3 +31,64 @@ var waterBlocks = function (blocks) {
     return pre + curr;
   }, 0);
 };
+
+var MedianStream = function () {
+  this._storage = [];
+  this._minHeap = [];
+  this._maxHeap = [];
+  this._compare = function (i, j) { return i < j };
+};
+
+MedianStream.prototype = {
+  insert: function (value) {
+    if(this._storage.length < 2) {
+      this._storage.push(value);
+      if (this._storage.length === 2) {
+        if (this._compare(this._storage[0], this._storage[1])) {
+          this._maxHeap.push(this._storage[0]);
+          this._minHeap.push(this._storage[1]);
+        } else {
+          this._maxHeap.push(this._storage[1]);
+          this._minHeap.push(this._storage[0]);
+        }
+      }
+    } else {
+      if(this._maxHeap[0] < value) {
+        this._minHeap.push(value);
+        var minChildIndex = this._minHeap.length-1;
+        var minParentIndex = Math.floor((minChildIndex-1)/2);
+        while (!!this._minHeap[minParentIndex] && this._compare(this._minHeap[minChildIndex], this._minHeap[minParentIndex])) {
+          this.swap(minChildIndex, minParentIndex, this._minHeap);
+          minChildIndex = minParentIndex;
+          minParentIndex = Math.floor((minChildIndex-1)/2);
+        }
+      } else {
+        this._maxHeap.push(value);
+        var childIndex = this._maxHeap.length-1;
+        var parentIndex = Math.floor((childIndex-1)/2);
+        while (!!this._maxHeap[parentIndex] && this._compare(this._maxHeap[parentIndex], this._maxHeap[childIndex])) {
+          this.swap(childIndex, parentIndex, this._maxHeap);
+          childIndex = parentIndex;
+          parentIndex = Math.floor((childIndex-1)/2);
+        }
+      }
+    }
+
+  },
+  peekMedian: function () {
+    if (this._storage.length < 2) {
+      return this._storage[0];
+    }
+    if (this._maxHeap.length === this._minHeap.length) {
+      return (this._maxHeap[0] + this._minHeap[0]) / 2;
+    } else {
+      return this._maxHeap.length > this._minHeap.length ? this._maxHeap[0] : this._minHeap[0];
+    }
+  },
+  size: function () {return this._minHeap.length + this.maxHeap.length},
+  swap: function(childIndex, parentIndex, heap) {
+      var temp = this._heap[parentIndex];
+      this._heap[parentIndex] = this._heap[childIndex];
+      this._heap[childIndex] = temp;
+    }
+};
