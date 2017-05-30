c30ef33c222a9be92b812dece1e7a10a618da85d
hxue920 <devilsing@gmail.com>
2017-05-30 01:57:20 -0700

    add solution for bubble sort

--- src/hr_challenge.js
+++ src/hr_challenge.js
@@ -104,3 +104,28 @@ nthFibonacci = function(n) {
   return current;
 };
 
+// Bubble Sort
+// Bubble sort is considered the most basic sorting algorithm in Computer Science. It works by starting at the first element of an array and comparing it to the second element:
+
+// If the first element is greater than the second element, it swaps the two.
+// It then compares the second to the third, and the third to the fourth, and so on.
+// In this way, the largest values ‘bubble’ to the end of the array.
+// Once it gets to the end of the array, it starts over and repeats the process until the array is sorted numerically.
+// Implement a function that takes an array and sorts it using this technique.
+
+var bubbleSort = function(array) {
+  var n = array.length;
+  var arrayCopy = array.slice();
+  var temp;
+  while (n > 0) {
+    for (var i = 0; i < array.length-1; i++) {
+      if (arrayCopy[i] > arrayCopy[i+1]) {
+        temp = arrayCopy[i];
+        arrayCopy[i] = arrayCopy[i+1];
+        arrayCopy[i+1] = temp;
+      }
+    }
+    n-=1;
+  }
+  return arrayCopy;
+};
