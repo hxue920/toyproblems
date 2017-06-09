81f2cf6e063a9e2bb759884e6315d5493f6a8465
hxue920 <devilsing@gmail.com>
2017-06-09 01:27:03 -0700

    add solution to compose, pipe

--- src/hr_challenge.js
+++ src/hr_challenge.js
@@ -508,3 +508,54 @@ Tree.prototype.removeChild = function(child){
     throw new Error('That node is not an immediate child of this tree');
   }
 };
+
+// Compose, Pipe
+// Implement the functions compose and pipe.
+
+// Compose
+// Compose should return a function that is the composition of a list of functions of arbitrary length. Each function is called on the return value of the function that follows.
+
+// You can think of compose as moving right to left through its arguments.
+
+// Example
+// var greet = function(name){ return 'hi: ' + name;}
+// var exclaim = function(statement) { return statement.toUpperCase() + '!';}
+
+// var welcome = compose(greet, exclaim);
+// welcome('phillip'); //=> 'hi: PHILLIP!'
+// Pipe:
+// Pipe composes a series of functions and returns the resulting function. Each function is called on the return value of the preceding function.
+
+// You can think of pipe as moving left to right through its arguments.
+
+// Example
+// var add2 = function(number){ return number + 2; }
+// var multiplyBy3 = function(number){ return number * 3; }
+
+// pipe(add2, multiplyBy3)(5) //=> 21
+// pipe(add2, multiplyBy3, multiplyBy3)(5) //=> 63
+// HINT: You should use the functions reduce() and reduceRight() in your solutions.
+
+var compose = function(){
+
+  var args = Array.prototype.slice.call(arguments);
+  return function () {
+    var args2 = Array.prototype.slice.call(arguments);
+    return args.reduceRight(function(memo, curr) {
+      memo = curr.call(this, memo);
+      return memo;
+    }, args2[0]);
+  }
+};
+
+var pipe = function(){
+
+  var args = Array.prototype.slice.call(arguments);
+  return function () {
+    var args2 = Array.prototype.slice.call(arguments);
+    return args.reduce(function(memo, curr) {
+      memo = curr.call(this, memo);
+      return memo;
+    }, args2[0]);
+  }
+};
