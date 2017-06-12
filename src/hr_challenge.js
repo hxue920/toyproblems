2d4ede9eb555dae7539c88e6e78f1c93a12458a8
hxue920 <devilsing@gmail.com>
2017-06-12 00:01:41 -0700

    add solution for async map

--- src/hr_challenge.js
+++ src/hr_challenge.js
@@ -685,3 +685,32 @@ function isBalanced (str) {
   }
   return stack.length===0;
 }
+
+// Async Map
+// Implement the function asyncMap:
+
+// asyncMap has two parameters, an array of asynchronous functions (tasks) and a callback.
+// Each of the tasks takes a separate callback and invokes that callback when complete.
+
+// The callback passed to asyncMap is then performed once on the array of results of the callbacks of the tasks.
+
+// The order of these results should be the same as the order of the tasks.
+// It is important to note that this is not the order in which the tasks return,
+// but the order in which they are passed to asyncMap.
+
+// Once all the callbacks of the tasks are returned, asyncMap should invoke the callback
+// on the results array.
+
+var asyncMap = function(tasks, callback){
+  var resultArr = [];
+  var resultCount = 0;
+  for (let i = 0; i< tasks.length; i++) {
+    tasks[i](function(result) {
+      resultArr[i] = result;
+      resultCount+=1;
+      if(resultCount === tasks.length){
+        callback(resultArr);
+      }
+    });
+  }
+};
