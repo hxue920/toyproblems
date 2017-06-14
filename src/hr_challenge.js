217aa0b8f4bfede2541cec5785ea064be29ed40b
hxue920 <devilsing@gmail.com>
2017-06-13 23:50:13 -0700

    add solution to merge sort

--- src/hr_challenge.js
+++ src/hr_challenge.js
@@ -740,3 +740,37 @@ function longestRun (string) {
 
   return result;
 }
+
+// Merge Sort
+// Implement a function that sorts an array of numbers using the “mergesort” algorithm.
+
+// Mergesort uses a divide-and-conquer strategy. It begins by treating the input list of length N as a set of N “sublists” of length 1, which are considered to be sorted. Adjacent sublists are then “merged” into sorted sublists of length 2, which are merged into sorted sublists of length 4, and so on, until only a single sorted list remains. (Note, if N is odd, an extra sublist of length 1 will be left
+// after the first merge, and so on.)
+
+// This can be implemented using either a recursive (“top-down”) or an iterative (“bottom-up”) approach.
+
+function mergeSort(arr){
+  if (arr.length <= 1) {
+    return arr;
+  }
+  var split = Math.floor(arr.length/2);
+  var left = arr.slice(0, split);
+  var right = arr.slice(split);
+
+  function mergeSolution(left, right) {
+    var i = 0, j = 0, result = [];
+    while (left.length>i && right.length>j) {
+      if (left[i] < right[j]) {
+        result.push(left[i]);
+        i++;
+      } else {
+        result.push(right[j]);
+        j++;
+      }
+    }
+    var leftover = i === left.length? right.slice(j): left.slice(i);
+    return result.concat(leftover);
+  }
+
+  return mergeSolution(mergeSort(left), mergeSort(right));
+}
