0f38488ad19f3fb94b0c22dffbcbdf81d2d66890
hxue920 <devilsing@gmail.com>
2017-06-16 00:01:42 -0700

    add examples for rotate matrix

--- src/hr_challenge.js
+++ src/hr_challenge.js
@@ -892,6 +892,17 @@ function fractionConverter (number) {
 // Important note:
 // In mathematics, and generally in CS, matrices are identified as m-by-n, where m is the number of rows and n is the number of columns. So an [i][j] address in a matrix will be i places down, and j places over. This usually matches the way arrays are addressed in code, but keep in mind that it differs from use in geometry and computer graphics, where coordinates of the form (x,y) are usually x units over, and y units down.
 
+// Examples
+// Input Output
+// matrix:
+// [ [ 1, 2, 3, 4 ], [ 5, 6, 7, 8 ], [ 9, "A", "B", "C" ], [ "D", "E", "F", "G" ] ]  [ [ "D", 9, 5, 1 ], [ "E", "A", 6, 2 ], [ "F", "B", 7, 3 ], [ "G", "C", 8, 4 ] ]
+// matrix:
+// [ [ 1 ] ] [ [ 1 ] ]
+// matrix:
+// [ [ 1, 2 ], [ 3, 4 ] ]  [ [ 3, 1 ], [ 4, 2 ] ]
+// matrix:
+// [ [ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ] ] [ [ 7, 4, 1 ], [ 8, 5, 2 ], [ 9, 6, 3 ] ]
+
 function rotateMatrix (matrix) {
   var resultArr = [];
   for (var i=0; i<matrix[0].length; i++) {
