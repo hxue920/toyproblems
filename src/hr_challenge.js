002705436b01fcf8fdef6ac6804a7649e2f24555
hxue920 <devilsing@gmail.com>
2017-06-19 02:33:59 -0700

    add soluton for hash table

--- src/hr_challenge.js
+++ src/hr_challenge.js
@@ -983,3 +983,65 @@ function translateRomanNumeral (romanNumeral) {
   }
   return sum;
 }
+
+// Hash Table
+// Finish this implementation of a Hash Table. It should have the methods insert(), retrieve(), and remove(). Your hash table need not resize itself, but shall handle collisions.
+var makeHashTable = function(){
+    var table = {};
+    var storage = [];
+    var storageLimit = 1000;
+
+    table.insert = function(key, value){
+      var index = getIndexBelowMaxForKey(key, storageLimit);
+      storage[index] = storage[index] || [];
+      //YOUR CODE HERE
+      for (var i = 0; i < storage[index].length; i++) {
+        if(storage[index][i][0] === key) {
+          var preValue = storage[index][i][1];
+          storage[index][i][1] = value;
+          return preValue;
+        }
+      }
+      storage[index].push([key, value]);
+    };
+
+    table.retrieve = function(key){
+      //YOUR CODE HERE
+      var index = getIndexBelowMaxForKey(key, storageLimit);
+      storage[index] = storage[index] || [];
+      if (storage[index] === []){
+        return undefined;
+      } else {
+        for (var i = 0; i < storage[index].length; i++) {
+          if (storage[index][i][0] === key) {
+            return storage[index][i][1];
+          }
+        }
+      }
+    };
+
+    table.remove = function(key){
+      //YOUR CODE HERE
+      var index = getIndexBelowMaxForKey(key, storageLimit);
+      storage[index] = storage[index] || [];
+      if (storage[index] === []){
+        return undefined;
+      } else {
+        for (var i = 0; i < storage[index].length; i++) {
+          if (storage[index][i][0] === key) {
+            storage[index].splice(i, 1);
+          }
+        }
+      }
+    }
+    return table;
+  };
+ var getIndexBelowMaxForKey = function(str, max){
+   var hash = 0;
+   for (var i = 0; i < str.length; i++) {
+     hash = (hash<<5) + hash + str.charCodeAt(i);
+     hash = hash & hash; // Convert to 32bit integer
+     hash = Math.abs(hash);
+   }
+   return hash % max;
+ };
