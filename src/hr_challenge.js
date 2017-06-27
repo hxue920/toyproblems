694fbfee7e9f53d236be869fdfb44fc0ce886931
hxue920 <devilsing@gmail.com>
2017-06-27 02:18:32 -0700

    add solution to prime tester

--- src/hr_challenge.js
+++ src/hr_challenge.js
@@ -1391,3 +1391,32 @@ Tree.prototype.removeChild = function(child){
     throw new Error("That node is not an immediate child of this tree");
   }
 };
+
+// Prime Tester
+// A prime number is an integer greater than 1 that has no divisors other than itself and 1. Write a function that accepts a number and returns true if it’s a prime number, false if it’s not. The grader is looking at the efficiency of your solution (number of operations) as well as correctness!
+
+
+// Parameters:
+
+
+// n (required) - a number.
+
+function primeTester (n) {
+  // Write your code here, and
+  // return your final answer.
+  if (n <= 1) {
+    return false;
+  } else if (n <= 3) {
+    return true;
+  } else if (n % 2 === 0 || n % 3 === 0) {
+    return false;
+  }
+  let i = 5;
+  while (i*i <= n) {
+    if (n % i === 0 || n % (i + 2) === 0) {
+      return false
+    }
+    i += 6;
+  }
+  return true;
+}
