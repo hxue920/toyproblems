5de1ffea1f65cf97585ad90989ca5cd25e8f5908
hxue920 <devilsing@gmail.com>
2017-06-29 00:10:51 -0700

    add solution to insertion sort

--- src/hr_challenge.js
+++ src/hr_challenge.js
@@ -1454,3 +1454,42 @@ function range(begin, end) {
   }
   return result;
 }
+
+// Insertion Sort
+// Insertion sort is a basic sorting algorithm.
+
+// Insertion sort iterates over an array, growing a sorted array behind the current location. It takes each element from the input and finds the spot, up to the current point, where that element belongs (in constant space). It does this until it gets to the end of the array.
+
+// Insertion sort should be implemented as a stable sort. This means that equal elements
+// should retain their relative order. Numbers, as primitives, give us no way to check this,
+// so we’ll be sorting objects with a value field, on which they will be sorted, like so:
+
+// [{value: 10}, {value: 5, order: 1}, {value: 5, order: 2}]
+
+// A stable sort must return {value: 5, order: 1}, {value:5, order: 2} in that order.
+
+// EXTRA CREDIT:
+
+// Refactor your sort to (optionally) take an explicit comparator function as its second argument, so that callers can define arbitrary ways to sort elements. See Array.prototype.sort for an example of how this works (excerpt below):
+
+// If comparator(a, b) is less than 0, sort a to a lower index than b, i.e. a comes first.
+// If comparator(a, b) returns 0, leave a and b unchanged with respect to each other, but sorted with respect to all different elements.
+// If comparator(a, b) is greater than 0, sort b to a lower index than a.
+
+// If no comparator is given, just sort the elements using < or >.
+function insertionSort (array) {
+
+  for (var i = 1; i<array.length; i++) {
+    var j = i;
+    while (j>0 && array[j].value < array[j-1].value) {
+      swap(array, j-1, j);
+      j -= 1;
+    }
+  }
+  return array;
+}
+function swap (array, index1, index2) {
+  var temp = array[index1];
+  array[index1] = array[index2];
+  array[index2] = temp;
+}
