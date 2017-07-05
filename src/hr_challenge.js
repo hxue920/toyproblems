2ec7e14672b73c6158921e04264dc56849ace13f
hxue920 <devilsing@gmail.com>
2017-07-05 00:08:05 -0700

    add solution to coin sum

--- src/hr_challenge.js
+++ src/hr_challenge.js
@@ -1735,3 +1735,47 @@ Function.prototype.bind = function(context) {
     return result;
   }
 };
+
+// Coin Sums
+// In England the currency is made up of pound, £, and pence, p, and there are eight coins in general circulation:
+
+// 1p
+// 2p
+// 5p
+// 10p
+// 20p
+// 50p
+// £1 (100p)
+// £2 (200p)
+// Given a given number of pence, return the possible number of ways someone could make change.
+
+// It is possible to make 5 pence in the following ways:
+
+// 5 * 1p
+
+// 3 * 1p + 1 * 2p
+
+// 1 * 1p + 2 * 2p
+
+// 1 * 5p
+// In other words, find all the possible combinations of coins that sum to a given pence value.
+
+function coinSums (total) {
+
+  var count = 0;
+  function findSolution(reminder, index) {
+    var currCoin = coins[index];
+    if (index === 0) {
+      if (reminder%currCoin === 0) {
+        count+=1;
+        return;
+      }
+    }
+    while (reminder >= 0) {
+      findSolution(reminder, index-1);
+      reminder -= currCoin;
+    }
+  }
+  findSolution(total, coins.length-1);
+  return count;
+}
