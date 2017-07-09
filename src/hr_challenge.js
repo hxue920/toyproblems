87219f03f2dd3a29e93dbdafe921d7beb8a219f1
hxue920 <devilsing@gmail.com>
2017-07-09 01:04:13 -0700

    add solution to linked list cycles

--- src/hr_challenge.js
+++ src/hr_challenge.js
@@ -1956,3 +1956,41 @@ var getIndexBelowMaxForKey = function(str, max){
   }
   return hash % max;
 };
+
+// Linked List Cycles
+// Write a function that returns true if a linked list contains a cycle, or false if it terminates somewhere.
+
+// Explanation:
+
+// Generally, we assume that a linked list will terminate in a null next pointer, as follows:
+
+//  A -> B -> C -> D -> E -> null
+// A ‘cycle’ in a linked list is when traversing the list would result in visiting the same nodes over and over.
+
+// This is caused by pointing a node in the list to another node that already appeared earlier in the list. Example:
+
+//  A -> B -> C
+//       ^    |
+//       |    v
+//       E <- D
+// 'Constraint 1: Do this in linear time'
+
+// 'Constraint 2: Do this in constant space'
+
+// 'Constraint 3: Do not mutate the original nodes in any way'
+
+var hasCycle = function(linkedList){
+  var ahead = linkedList;
+  var behind = linkedList;
+  var move = false;
+  while (ahead = ahead.next) {
+    if (ahead === behind) {
+      return true;
+    }
+    if (move) {
+      behind = behind.next;
+    }
+    move = !move;
+  }
+  return false
+};
