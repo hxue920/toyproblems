81a01bc247c238ffd4782cae9e20547c461694b0
hxue920 <devilsing@gmail.com>
2017-07-15 00:07:31 -0700

    add solution to spiral traversal

--- src/hr_challenge.js
+++ src/hr_challenge.js
@@ -2140,3 +2140,43 @@ function getPonyAllergies (ponies, ownerEmail) {
     return accu;
   },[]);
 }
+
+// Spiral Traversal
+// Write a function that accepts a 2-dimensional array (that is, an array containing many same-length arrays),
+// and prints out every value found, but in a spiral from the upper left in to the center.
+
+function spiralTraversal (matrix) {
+  var result = [];
+  var dir = top = left = 0;
+  var bottom = matrix.length - 1;
+  var right = matrix[0].length - 1;
+
+  while (top <= bottom && left <= right) {
+    if (dir === 0) {
+      for (var i = left; i <= right; i++) {
+        result.push(matrix[top][i]);
+      }
+      top += 1;
+      dir = 1;
+    } else if (dir === 1) {
+      for (var i = top; i <= bottom; i++) {
+        result.push(matrix[i][right]);
+      }
+      right -= 1;
+      dir = 2;
+    } else if (dir === 2) {
+      for (var i = right; i >= left; i-- ) {
+        result.push(matrix[bottom][i]);
+      }
+      bottom -= 1;
+      dir = 3;
+    } else if (dir === 3) {
+      for (var i = bottom; i >= top; i--) {
+        result.push(matrix[i][left]);
+      }
+      left += 1;
+      dir = 0;
+    }
+  }
+  return result;
+}
