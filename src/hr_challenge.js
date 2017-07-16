bbf876b64b4864623e068d6173cf95629107df59
hxue920 <devilsing@gmail.com>
2017-07-16 00:01:46 -0700

    add solution to common ancestor

--- src/hr_challenge.js
+++ src/hr_challenge.js
@@ -2180,3 +2180,115 @@ function spiralTraversal (matrix) {
   }
   return result;
 }
+
+// Common Ancestor
+// Implement the function getClosestCommonAncestor and getAncestorPath in the Tree class
+
+// // Valid Child
+// var grandma = new Tree();
+// var mom = new Tree();
+// var uncle = new Tree();
+// grandma.addChild(mom);
+// grandma.addChild(uncle);
+// var me = new Tree();
+// mom.addChild(me);
+
+// grandma.getAncestorPath(me); // => [grandma, mom, me]
+// mom.getAncestorPath(me)  // => [mom, me]
+// me.getAncestorPath(me) // => [me]
+// grandma.getClosestCommonAncestor(me, uncle); // => grandma
+
+// // Invalid Child
+// grandma.getAncestorPath(H R Giger) // => null
+
+var Tree = function(){
+  this.children = [];
+};
+
+/**
+  * add an immediate child
+  */
+Tree.prototype.addChild = function(child){
+  if(!this.isDescendant(child)){
+    this.children.push(child);
+  }else {
+    throw new Error("That child is already a child of this tree");
+  }
+  return this;
+};
+
+Tree.prototype.getClosestCommonAncestor = function(){
+  // TODO: implement me!
+  path1 = this.getAncestorPath(arguments[0]);
+  path2 = this.getAncestorPath(arguments[1]);
+  if (path1===null || path2===null) {
+    return null
+  }
+  for (var i = Math.min(path1.length-1, path2.length-1); i >= 0; i--) {
+    if (path2.includes(path1[i])) {
+      return path1[i];
+    }
+  }
+}
+
+Tree.prototype.getAncestorPath = function(target){
+  // TODO: implement me!
+  if (this === target) {
+    return [this];
+  }
+  for (var i = 0; i < this.children.length; i++) {
+    var path = this.children[i].getAncestorPath(target);
+    if (path) {
+      return [this].concat(path);
+    }
+  }
+  // var result = [];
+  // function findSolution(tree) {
+  //   if (tree === target) {
+  //     result.push(tree);
+  //     return;
+  //   }
+
+  //   for (var i = 0; i < tree.children.length; i++) {
+  //     if (tree.children[i].isDescendant(target)) {
+  //       result.push(tree.children[i]);
+  //       findSolution(tree.children[i])
+  //     }
+  //   }
+  // }
+  // findSolution(this);
+  // return result;
+  return null;
+}
+
+/**
+  * check to see if the provided tree is already a child of this
+  * tree __or any of its sub trees__
+  */
+Tree.prototype.isDescendant = function(child){
+  if(this.children.indexOf(child) !== -1){
+    // `child` is an immediate child of this tree
+    return true;
+  }else{
+    for(var i = 0; i < this.children.length; i++){
+      if(this.children[i].isDescendant(child)){
+        // `child` is descendant of this tree
+        return true;
+      }
+    }
+    return false;
+  }
+};
+
+/**
+  * remove an immediate child
+  */
+Tree.prototype.removeChild = function(child){
+  var index = this.children.indexOf(child);
+  if(index !== -1){
+    // remove the child
+    this.children.splice(index,1);
+  }else{
+    throw new Error("That node is not an immediate child of this tree");
+  }
+};
