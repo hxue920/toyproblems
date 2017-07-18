6af9122e328051702ab853ab00d0258f544bab9f
hxue920 <devilsing@gmail.com>
2017-07-17 19:24:38 -0700

    add solution to numbers to words

--- src/hr_challenge.js
+++ src/hr_challenge.js
@@ -2292,3 +2292,74 @@ Tree.prototype.removeChild = function(child){
     throw new Error("That node is not an immediate child of this tree");
   }
 };
+
+// Number to English
+// Write a function numberToEnglish, it should take a number and return the number as a string using English words.
+var numbersToWords = {
+  0: 'zero',
+  1: 'one',
+  2: 'two',
+  3: 'three',
+  4: 'four',
+  5: 'five',
+  6: 'six',
+  7: 'seven',
+  8: 'eight',
+  9: 'nine',
+  10: 'ten',
+  11: 'eleven',
+  12: 'twelve',
+  13: 'thirteen',
+  14: 'fourteen',
+  15: 'fifteen',
+  16: 'sixteen',
+  17: 'seventeen',
+  18: 'eighteen',
+  19: 'nineteen',
+  20: 'twenty',
+  30: 'thirty',
+  40: 'forty',
+  50: 'fifty',
+  60: 'sixty',
+  70: 'seventy',
+  80: 'eighty',
+  90: 'ninety',
+};
+var numbersToPlace = {
+  10: 'ten',
+  100: 'hundred',
+  1000: 'thousand',
+  1000000: 'million',
+  1000000000: 'billion',
+  1000000000000: 'trillion',
+  1000000000000000: 'quadrillion',
+  1000000000000000000: 'quintillion',
+};
+
+function numberToEnglish (num) {
+  var n = num, output, numInPlace, numLeft, place, restOfString;
+  if (numbersToWords[n]) {
+    output = numbersToWords[n];
+  } else if (n < 100) {
+    numInPlace = Math.floor(n / 10);
+    numLeft = n % 10;
+    output = numbersToWords[numInPlace * 10] +'-'+ numberToEnglish(numLeft);
+  } else {
+    if (n < 1000) {
+      place = 100;
+    } else {
+      place = 1000;
+      while (place * 1000 <= n) {
+        place *= 1000;
+      }
+    }
+    numInPlace = Math.floor(n / place);
+    numLeft = n % place;
+    output = numberToEnglish(numInPlace) +' '+ numbersToPlace[place];
+    restOfString = numberToEnglish(numLeft);
+    if (restOfString !== 'zero') {
+      output += ' ' + restOfString;
+    }
+  }
+  return output
+}
