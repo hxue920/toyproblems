69f33158f1d9feefb03ee00789cb72a5ab44dc79
hxue920 <devilsing@gmail.com>
2017-07-18 22:21:32 -0700

    add initial base code for queue stack solution

--- src/week14.js
+++ src/week14.js
@@ -118,3 +118,19 @@ function main() {
     }
 
 }
+
+// Queues: A Tale of Two Stacks
+function processData(input) {
+    //Enter your code here
+}
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
