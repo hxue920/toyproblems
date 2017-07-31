8294b812c4d65712e0a1cf089d7868b4338228ca
hxue920 <devilsing@gmail.com>
2017-07-30 22:10:33 -0700

    add starter code for merge sort hackerrank solution

--- src/week14.js
+++ src/week14.js
@@ -238,3 +238,36 @@ function main() {
                 First Element: ${a[0]}
                 Last Element: ${a[a.length-1]}`);
 }
+
+//Merge Sort
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var t = parseInt(readLine());
+    for(var a0 = 0; a0 < t; a0++){
+        var n = parseInt(readLine());
+        arr = readLine().split(' ');
+        arr = arr.map(Number);
+    }
+
+}
