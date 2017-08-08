6b34afda549284e440e1ca208903eb89380b051f
hxue920 <devilsing@gmail.com>
2017-08-07 22:29:48 -0700

    add base code for BFS shortest reach

--- src/week14.js
+++ src/week14.js
@@ -378,3 +378,19 @@ function main() {
     connectedCells();
 
 }
+
+//BFS: Shortest Reach in a Graph
+function processData(input) {
+    //Enter your code here
+}
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
