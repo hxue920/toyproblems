1f5a910627f0fe6ca1280e409c689397bca5b0cd
hxue920 <devilsing@gmail.com>
2017-08-08 23:41:47 -0700

    add base code for time complexity: primality solution

--- src/week14.js
+++ src/week14.js
@@ -394,3 +394,33 @@ process.stdin.on("data", function (input) {
 process.stdin.on("end", function () {
    processData(_input);
 });
+
+//Time Complexity: Primality
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var p = parseInt(readLine());
+    for(var a0 = 0; a0 < p; a0++){
+        var n = parseInt(readLine());
+
+}
