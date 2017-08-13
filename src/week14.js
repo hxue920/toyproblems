75d0c98f2ccfd071b4cd2a181f7b9b8ffa34cc74
hxue920 <devilsing@gmail.com>
2017-08-12 17:44:43 -0700

    add base code for Recursion: Davis Staircase

--- src/week14.js
+++ src/week14.js
@@ -461,3 +461,34 @@ function fibonacci(n) {
     if(n === 1 || n === 2) return 1;
     return fibonacci(n-1) + fibonacci(n-2);
 }
+
+//Recursion: Davis' Staircase
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var s = parseInt(readLine());
+    for(var a0 = 0; a0 < s; a0++){
+        var n = parseInt(readLine());
+    }
+
+}
