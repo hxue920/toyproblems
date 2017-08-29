5771a07a7c48225bcb9ea1e0b237eea5dc546de5
hxue920 <devilsing@gmail.com>
2017-08-28 23:49:30 -0700

    add base code for Between Two Sets

--- src/week14.js
+++ src/week14.js
@@ -730,3 +730,43 @@ function main() {
     process.stdout.write("" + result + "\n");
 
 }
+
+//Between Two Sets
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function getTotalX(a, b) {
+    // Complete this function
+}
+
+function main() {
+    var n_temp = readLine().split(' ');
+    var n = parseInt(n_temp[0]);
+    var m = parseInt(n_temp[1]);
+    a = readLine().split(' ');
+    a = a.map(Number);
+    b = readLine().split(' ');
+    b = b.map(Number);
+    var total = getTotalX(a, b);
+    process.stdout.write("" + total + "\n");
+
+}
