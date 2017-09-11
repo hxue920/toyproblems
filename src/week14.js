acd7747a39532472c12e8837c4fbd9f29f3b461f
hxue920 <devilsing@gmail.com>
2017-09-11 12:36:17 -0700

    add base code for Day of the Programmer

--- src/week14.js
+++ src/week14.js
@@ -978,3 +978,37 @@ function main() {
     process.stdout.write("" + result + "\n");
 
 }
+
+//Day of the Programmer
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function solve(year){
+    // Complete this function
+}
+
+function main() {
+    var year = parseInt(readLine());
+    var result = solve(year);
+    process.stdout.write(""+result+"\n");
+
+}
