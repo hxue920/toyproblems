c4b939c35019e727bbcad0c9094ac4b93de3fa7d
hxue920 <devilsing@gmail.com>
2017-09-12 15:34:44 -0700

    add solution to Day of the Proogrammer

--- src/week14.js
+++ src/week14.js
@@ -1003,8 +1003,18 @@ function readLine() {
 /////////////// ignore above this line ////////////////////
 
 function solve(year){
-    // Complete this function
-}
+    if (year === 1918) {
+        return '26.09.1918';
+    }
+    if (year < 1918) {
+        if (year % 4 === 0) {
+            return '12.09.'+ year;
+        }
+        return '13.09.'+year;
+    } else if (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)) {
+        return '12.09.'+ year;
+    }
+    return '13.09.'+year;
 
 function main() {
     var year = parseInt(readLine());
