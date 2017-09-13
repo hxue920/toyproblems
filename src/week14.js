4643feb89d728610e4bf9305d6d6c4c629843cc8
hxue920 <devilsing@gmail.com>
2017-09-13 00:02:15 -0700

    refactored Day of the Programmer

--- src/week14.js
+++ src/week14.js
@@ -1006,15 +1006,11 @@ function solve(year){
     if (year === 1918) {
         return '26.09.1918';
     }
-    if (year < 1918) {
-        if (year % 4 === 0) {
-            return '12.09.'+ year;
-        }
-        return '13.09.'+year;
-    } else if (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)) {
+    if ((year < 1918 && year % 4 === 0) || year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0) ) {
         return '12.09.'+ year;
+    } else {
+        return '13.09.'+ year;
     }
-    return '13.09.'+year;
 
 function main() {
     var year = parseInt(readLine());
