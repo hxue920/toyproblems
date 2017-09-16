40c02597e6d9ae2283aaad8fea3d7496ce3a79f8
hxue920 <devilsing@gmail.com>
2017-09-15 20:39:20 -0700

    add base code for Bon Appétit

--- src/week14.js
+++ src/week14.js
@@ -1028,3 +1028,42 @@ var twoSum = function(nums, target) {
         }
     }
 };
+
+//Bon Appétit
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function bonAppetit(n, k, b, ar) {
+    // Complete this function
+}
+
+function main() {
+    var n_temp = readLine().split(' ');
+    var n = parseInt(n_temp[0]);
+    var k = parseInt(n_temp[1]);
+    ar = readLine().split(' ');
+    ar = ar.map(Number);
+    var b = parseInt(readLine());
+    var result = bonAppetit(n, k, b, ar);
+    process.stdout.write("" + result + "\n");
+
+}
