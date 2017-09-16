94cd7c263ceff15ea563f938e0b5b914152be393
hxue920 <devilsing@gmail.com>
2017-09-16 00:03:59 -0700

    add base code for sock merchant

--- src/week14.js
+++ src/week14.js
@@ -1067,3 +1067,39 @@ function main() {
     process.stdout.write("" + result + "\n");
 
 }
+
+//Sock Merchant
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function sockMerchant(n, ar) {
+    // Complete this function
+}
+
+function main() {
+    var n = parseInt(readLine());
+    ar = readLine().split(' ');
+    ar = ar.map(Number);
+    var result = sockMerchant(n, ar);
+    process.stdout.write("" + result + "\n");
+
+}
