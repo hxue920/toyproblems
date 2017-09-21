fd5c90940a499caaf49477299ed53f605bc4ac98
hxue920 <devilsing@gmail.com>
2017-09-21 16:56:55 -0700

    add base code for counting valleys

--- src/week14.js
+++ src/week14.js
@@ -1158,3 +1158,19 @@ function main() {
     process.stdout.write(""+result+"\n");
 
 }
+
+//Counting Valleys
+function processData(input) {
+    //Enter your code here
+}
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
