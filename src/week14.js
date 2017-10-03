0dbf82fc3fae9209ecdba082aec5c8c7e45fd3c9
hxue920 <devilsing@gmail.com>
2017-10-02 22:11:33 -0700

    add base code for climbing the leaderboard

--- src/week14.js
+++ src/week14.js
@@ -1396,3 +1396,37 @@ function main() {
     console.log(result);
 
 }
+
+//Climbing the Leaderboard
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var n = parseInt(readLine());
+    scores = readLine().split(' ');
+    scores = scores.map(Number);
+    var m = parseInt(readLine());
+    alice = readLine().split(' ');
+    alice = alice.map(Number);
+    // your code goes here
+
+}
