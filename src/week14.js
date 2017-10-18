956425c15709c6ebfa580f755d337613b6d9d7ad
hxue920 <devilsing@gmail.com>
2017-10-17 20:50:54 -0700

    add base code to strange advertising

--- src/week14.js
+++ src/week14.js
@@ -1693,3 +1693,19 @@ process.stdin.on("data", function (input) {
 process.stdin.on("end", function () {
    processData(_input);
 });
+
+//
+function processData(input) {
+
+}
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
