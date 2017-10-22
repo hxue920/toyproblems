f80441cf3d4557514ef7cce24a1bddb45654493f
hxue920 <devilsing@gmail.com>
2017-10-21 21:18:14 -0700

    add base code for save the prisoner

--- src/week14.js
+++ src/week14.js
@@ -1742,3 +1742,43 @@ process.stdin.on("data", function (input) {
 process.stdin.on("end", function () {
    processData(_input);
 });
+
+//Save the Prisoner
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function saveThePrisoner(n, m, s){
+    // Complete this function
+}
+
+function main() {
+    var t = parseInt(readLine());
+    for(var a0 = 0; a0 < t; a0++){
+        var n_temp = readLine().split(' ');
+        var n = parseInt(n_temp[0]);
+        var m = parseInt(n_temp[1]);
+        var s = parseInt(n_temp[2]);
+        var result = saveThePrisoner(n, m, s);
+        process.stdout.write(""+result+"\n");
+    }
+
+}
