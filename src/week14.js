29f8480e350b7d6fcd69cb6c91e0a031b9116851
hxue920 <devilsing@gmail.com>
2017-10-24 23:44:48 -0700

    add base code for jumping on the clouds revisited

--- src/week14.js
+++ src/week14.js
@@ -1816,3 +1816,35 @@ process.stdin.on("data", function (chunk) {
 process.stdin.on("end", function () {
     // now we can read/parse input
 });
+
+//jumping on the clouds revisited
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var n_temp = readLine().split(' ');
+    var n = parseInt(n_temp[0]);
+    var k = parseInt(n_temp[1]);
+    c = readLine().split(' ');
+    c = c.map(Number);
+
+}
