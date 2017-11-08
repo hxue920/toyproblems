1c598498082908851f6f907da66d9fc36412143d
hxue920 <devilsing@gmail.com>
2017-11-07 23:54:17 -0800

    add base code for fight digits

--- src/week14.js
+++ src/week14.js
@@ -1849,6 +1849,38 @@ function main() {
 
 }
 
+//find digits
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var t = parseInt(readLine());
+    for(var a0 = 0; a0 < t; a0++){
+        var n = parseInt(readLine());
+    }
+
+}
+
+
 // price window subranges
 process.stdin.resume();
 process.stdin.setEncoding('ascii');
@@ -1985,3 +2017,4 @@ function subrangeDiffOptimized(input) {
     }
 
 }
+
