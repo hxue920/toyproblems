871eed931db6026f6fc0331f9e6d632c11c541ff
hxue920 <devilsing@gmail.com>
2017-11-10 23:46:14 -0800

    add base code to appemd amd delete

--- src/week14.js
+++ src/week14.js
@@ -1908,6 +1908,36 @@ function main() {
 
 }
 
+//Append and Delete
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var s = readLine();
+    var t = readLine();
+    var k = parseInt(readLine());
+
+}
+
 
 // price window subranges
 process.stdin.resume();
