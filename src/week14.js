00b55a5852ccdc969df3dd2e4b90e8044cc410c8
hxue920 <devilsing@gmail.com>
2017-11-11 23:47:18 -0800

    add base code for library fine

--- src/week14.js
+++ src/week14.js
@@ -1938,6 +1938,40 @@ function main() {
 
 }
 
+//Library Fine
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var d1_temp = readLine().split(' ');
+    var d1 = parseInt(d1_temp[0]);
+    var m1 = parseInt(d1_temp[1]);
+    var y1 = parseInt(d1_temp[2]);
+    var d2_temp = readLine().split(' ');
+    var d2 = parseInt(d2_temp[0]);
+    var m2 = parseInt(d2_temp[1]);
+    var y2 = parseInt(d2_temp[2]);
+
+}
 
 // price window subranges
 process.stdin.resume();
