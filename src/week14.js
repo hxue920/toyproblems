c226df2ced3c9f8655cbd59a0c8fc1eea3548788
hxue920 <devilsing@gmail.com>
2017-11-20 21:34:49 -0800

    add base code for cut the sticks

--- src/week14.js
+++ src/week14.js
@@ -2195,3 +2195,33 @@ function subrangeDiffOptimized(input) {
 function getSum(a, b) {
     return b === 0 ? a : getSum(a^b, (a&b) << 1);
 }
+
+//Cut the Sticks
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var n = parseInt(readLine());
+    arr = readLine().split(' ');
+    arr = arr.map(Number);
+
+}
