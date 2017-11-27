7db852757a6ec2a963cc8c7776797c5b09838b65
hxue920 <devilsing@gmail.com>
2017-11-26 21:27:41 -0800

    add base code for repeated string

--- src/week14.js
+++ src/week14.js
@@ -2284,3 +2284,32 @@ process.stdin.on("data", function (input) {
 process.stdin.on("end", function () {
    processData(_input);
 });
+
+//Repeated String
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var s = readLine();
+    var n = parseInt(readLine());
+
+}
