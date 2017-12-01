612d815282e2ea9644cc761f608a9ba96542040a
hxue920 <devilsing@gmail.com>
2017-11-30 23:08:29 -0800

    add solution to equalize the array

--- src/week14.js
+++ src/week14.js
@@ -2365,3 +2365,19 @@ function main() {
     }
     console.log(steps);
 }
+
+//Equalize the Array
+function processData(input) {
+    //Enter your code here
+}
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
