37659694784260150999da41d80b1ba0b0a6e99c
hxue920 <devilsing@gmail.com>
2017-12-05 22:53:56 -0800

    add base code for acm icpc team

--- src/week14.js
+++ src/week14.js
@@ -2490,3 +2490,38 @@ function checkDownRight(row, col, matrix, count) {
     count += 1;
     checkDownRight(row-1, col+1, matrix, count);
 }
+
+
+//ACM ICPC Team
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var n_temp = readLine().split(' ');
+    var n = parseInt(n_temp[0]);
+    var m = parseInt(n_temp[1]);
+    var topic = [];
+    for(var topic_i = 0; topic_i < n; topic_i++){
+       topic[topic_i] = readLine();
+    }
+
+}
