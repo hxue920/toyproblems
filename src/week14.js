5abf6ee71e8becc9cc716503e4da1857951a3375
hxue920 <devilsing@gmail.com>
2017-12-08 20:49:15 -0800

    add base code to taum and bday

--- src/week14.js
+++ src/week14.js
@@ -2542,3 +2542,40 @@ function main() {
     }
 
 }
+
+//Taum and B'day
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var t = parseInt(readLine());
+    for(var a0 = 0; a0 < t; a0++){
+        var b_temp = readLine().split(' ');
+        var b = parseInt(b_temp[0]);
+        var w = parseInt(b_temp[1]);
+        var x_temp = readLine().split(' ');
+        var x = parseInt(x_temp[0]);
+        var y = parseInt(x_temp[1]);
+        var z = parseInt(x_temp[2]);
+    }
+
+}
