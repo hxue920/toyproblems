33442696a85b8ae6d9b267593709a411962b8129
hxue920 <devilsing@gmail.com>
2017-12-16 23:41:10 -0800

    add base code for binary search: ice cream parlor

--- src/week14.js
+++ src/week14.js
@@ -2670,3 +2670,37 @@ process.stdin.on("data", function (input) {
 process.stdin.on("end", function () {
    processData(_input);
 });
+
+//Binary Search: Ice Cream Parlor
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var t = parseInt(readLine());
+    for(var a0 = 0; a0 < t; a0++){
+        var m = parseInt(readLine());
+        var n = parseInt(readLine());
+        a = readLine().split(' ');
+        a = a.map(Number);
+    }
+
+}
