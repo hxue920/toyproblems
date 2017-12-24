0e67fbb17a160277d95c4981a486c0e233efb135
hxue920 <devilsing@gmail.com>
2017-12-23 21:49:38 -0800

    add base code for heap: find the running median

--- src/week14.js
+++ src/week14.js
@@ -2803,3 +2803,35 @@ process.stdin.on("data", function (input) {
 process.stdin.on("end", function () {
    processData(_input);
 });
+
+//Heaps: Finding the Running Median
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var n = parseInt(readLine());
+    var a = [];
+    for(var a_i = 0; a_i < n; a_i++){
+       a[a_i] = parseInt(readLine());
+    }
+
+}
