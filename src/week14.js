8af61dc1eec08206a594eb123adb0994f16c49d5
hxue920 <devilsing@gmail.com>
2017-12-28 23:35:08 -0800

    add base code for tries: contacts

--- src/week14.js
+++ src/week14.js
@@ -2939,3 +2939,36 @@ class binaryHeap {
         }
     }
 }
+
+//Tries: Contacts
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var n = parseInt(readLine());
+    for(var a0 = 0; a0 < n; a0++){
+        var op_temp = readLine().split(' ');
+        var op = op_temp[0];
+        var contact = op_temp[1];
+    }
+
+}
