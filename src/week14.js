9ea42f83ea5ba3db1a5af832a15a80086de7802c
hxue920 <devilsing@gmail.com>
2017-12-29 23:07:05 -0800

    add base code for minimum distances

--- src/week14.js
+++ src/week14.js
@@ -2972,3 +2972,39 @@ function main() {
     }
 
 }
+
+//Minimum Distances
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function minimumDistances(a) {
+    // Complete this function
+}
+
+function main() {
+    var n = parseInt(readLine());
+    a = readLine().split(' ');
+    a = a.map(Number);
+    var result = minimumDistances(a);
+    process.stdout.write("" + result + "\n");
+
+}
