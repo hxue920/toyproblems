8af06f9923dbcff9b2671d563ad4f0cb01899e12
hxue920 <devilsing@gmail.com>
2018-01-04 19:21:20 -0800

    add base code to lisas workbook

--- src/week14.js
+++ src/week14.js
@@ -3111,3 +3111,41 @@ function main() {
     }
 
 }
+
+//Lisa's Workbook
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function workbook(n, k, arr) {
+    // Complete this function
+}
+
+function main() {
+    var n_temp = readLine().split(' ');
+    var n = parseInt(n_temp[0]);
+    var k = parseInt(n_temp[1]);
+    arr = readLine().split(' ');
+    arr = arr.map(Number);
+    var result = workbook(n, k, arr);
+    process.stdout.write("" + result + "\n");
+
+}
