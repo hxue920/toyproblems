d519f4c0b033b82e175c7160a08f03401c856827
hxue920 <devilsing@gmail.com>
2018-01-07 21:53:17 -0800

    add base code for fair rations

--- src/week14.js
+++ src/week14.js
@@ -3209,3 +3209,39 @@ function main() {
     process.stdout.write("" + result + "\n");
 
 }
+
+//Fair Rations
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function fairRations(B) {
+    // Complete this function
+}
+
+function main() {
+    var N = parseInt(readLine());
+    B = readLine().split(' ');
+    B = B.map(Number);
+    var result = fairRations(B);
+    process.stdout.write("" + result + "\n");
+
+}
