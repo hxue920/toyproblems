ed0d93e3979541f67c7544472b2d3df02dfc6bbe
hxue920 <devilsing@gmail.com>
2018-01-11 23:59:14 -0800

    add base code for cavity map

--- src/week14.js
+++ src/week14.js
@@ -3275,3 +3275,43 @@ function main() {
     process.stdout.write("" + result + "\n");
 
 }
+
+//Cavity Map
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function cavityMap(grid) {
+    // Complete this function
+}
+
+function main() {
+    var n = parseInt(readLine());
+    var grid = [];
+    for(var grid_i = 0; grid_i < n; grid_i++){
+       grid[grid_i] = readLine();
+    }
+    var result = cavityMap(grid);
+    console.log(result.join("\n"));
+
+
+
+}
