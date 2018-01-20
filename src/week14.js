fc7d9e0038710c9da1e8ff2d94cf7feefc6f9632
hxue920 <devilsing@gmail.com>
2018-01-19 23:59:08 -0800

    add base code for strange counter

--- src/week14.js
+++ src/week14.js
@@ -3386,3 +3386,37 @@ function main() {
     }
 
 }
+
+//Strange Counter
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function strangeCode(t) {
+    // Complete this function
+}
+
+function main() {
+    var t = parseInt(readLine());
+    var result = strangeCode(t);
+    process.stdout.write("" + result + "\n");
+
+}
